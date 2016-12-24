50f3c188c6157b6df97407e5da4b40b70532038c
Sergey Gornostaev <sergey1008@gmail.com>
2016-12-25 01:33:05 +0500

    изменил грантфайл

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,14 @@
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   require("load-grunt-tasks")(grunt);
-
 
   grunt.initConfig({
     less: {
       style: {
         files: {
-          "build/css/style.css": ["less/style.less"]
+          "build/css/style.css": "less/style.less"
         }
       }
     },
@@ -17,20 +16,36 @@ module.exports = function(grunt) {
     postcss: {
       style: {
         options: {
-          processors: [
-            require("autoprefixer")({browsers: [
-              "last 1 versions",
-              "last 2 Chrome versions",
-              "last 2 Firefox versions",
-              "last 2 Opera versions",
-              "last 2 Edge versions"
-            ]}),
-            require("css-mqpacker")({
-              sort: true
-            })
-          ]
+          processors: [require("autoprefixer")({browsers: ["last 2 versions"]})]
         },
         src: "build/css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: ["build/*.html", "build/css/*.css"]
+        },
+        options: {
+          server: "build",
+          watchTask: true,
+          notify: false,
+          open: true,
+          cors: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ["*.html"],
+        tasks: ["copy:html"] },
+
+      style: {
+        files: ["less/**/*.less"],
+        tasks: ["less", "postcss", "csso"]
       }
     },
 
@@ -41,8 +56,32 @@ module.exports = function(grunt) {
         },
         files: {
           "build/css/style.min.css": ["build/css/style.css"]
-          }
         }
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          opitimizationLabel: 3
+        },
+        files: [
+          {
+            expand: true,
+            src: ["build/img/**/*{png,jpg,gif}"]
+          }
+        ]
+      }
+    },
+
+    svgmin: {
+      symbols: {
+        files: [
+          {
+            expand: true,
+            src: ["build/img/icons/*.svg"]
+          }
+        ]
       }
     },
 
@@ -59,81 +98,35 @@ module.exports = function(grunt) {
       }
     },
 
-    svgmin: {
-      symbols: {
-        filex: [{
-          expand: true,
-          src: ["build/img/icons/*.svg"]
-        }]
-      }
-    },
-
-    imagemin: {
-      images: {
-        options: {
-          optimizationLevel: 3
-        },
-        files: [{
-          expand: true,
-          src: ["build/img/**/*.{png,jpg,gif}"]
-        }]
-      }
-    },
-
-    browserSync: {
-      server: {
-        bsFiles: {
-          src: ["build/*.html", "build/css/*.css"]
-        },
-        options: {
-          server: "build",
-          watchTask: true
-        }
-      }
-    },
-
-    watch: {
-      html: {
-        files: ["*.html"],
-        tasks: ["copy:html"]
-      },
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss", "csso"]
-      }
-    },
-
     copy: {
       build: {
-        files: [{
-          expand: true,
-          src: [
-            "fonts/**/*.{woff,woff2}",
-            "img/**",
-            "js/**",
-            "*.html"
-          ],
-          dest: "build"
-        }]
-      },
-      html: {
-        files: [{
-          expand: true,
-          src: ["*.html"],
-          dest: "build"
-        }]
+        files: [
+          {
+            expand: true,
+            src: [
+              "fonts/**/*.{woff, woff2}", "img/**", "js/**", "*.html"
+            ],
+            dest: "build"
+          }
+        ]
       }
     },
+
+    html: {
+      files: [{
+        expand: true,
+        src: ["*.html"],
+        dest: "build"
+      }] },
+
 
     clean: {
       build: ["build"]
     }
-
   });
 
   grunt.registerTask("serve", ["browserSync", "watch"]);
   grunt.registerTask("symbols", ["svgmin", "svgstore"]);
-
   grunt.registerTask("build", [
     "clean",
     "copy",
